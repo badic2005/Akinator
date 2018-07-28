13bf6e60452ce0a95726d23610957a99737ba38a
badic <vadim021703@gmail.com>
2018-07-28 22:56:27 +0300

    Start configuration with Spring Boot

---
+++ src/main/java/ru/nexusaqua/akinator/AkinatorApplication.java
@@ -0,0 +1,12 @@
+package ru.nexusaqua.akinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkinatorApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/nexusaqua/akinator/AkinatorApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.nexusaqua.akinator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AkinatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
