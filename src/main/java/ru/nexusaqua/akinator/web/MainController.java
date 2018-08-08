a0827095b90df325ff6a683403aff197a9a851a3
badic <vadim021703@gmail.com>
2018-08-08 23:40:59 +0300

    Deploy

--- src/main/java/ru/nexusaqua/akinator/web/MainController.java
+++ src/main/java/ru/nexusaqua/akinator/web/MainController.java
@@ -2,13 +2,12 @@ package ru.nexusaqua.akinator.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/")
     String home() {
-        return "index.html";
+        return "static/index.html";
     }
 }
