ac226db60e13cfb13d15364ad67cf7029d510ed2
badic <vadim021703@gmail.com>
2018-08-08 15:27:16 +0300

    fixing customers

--- src/main/java/ru/nexusaqua/akinator/web/customer/CustomerController.java
+++ src/main/java/ru/nexusaqua/akinator/web/customer/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import ru.nexusaqua.akinator.model.Customer;
 import ru.nexusaqua.akinator.repository.CustomerRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -16,64 +18,38 @@ public class CustomerController {
     private CustomerRepository customerRepository;
 
     @GetMapping
-    public  Iterable<Customer> getAll(@RequestParam(required = false) String filter, Model model) {
+    @ResponseBody
+    public Iterable<Customer> get(@RequestParam(required = false) final String filter) {
         // This returns a JSON or XML with the users
-        Iterable<Customer> customers;
+        Iterable<Customer> customers = new ArrayList<>();
 
         if(filter != null && !filter.isEmpty()) {
             customers = customerRepository.findAllByNameContaining(filter);
-        } else {
-            customers = customerRepository.findAll();
         }
-
-        model.addAttribute("customers", customers);
-        model.addAttribute("filter", filter);
 
         return customers;
     }
 
-    @GetMapping("{id}")
-    public  Customer get(@PathVariable String id, @RequestParam(required = false) String filter, Model model) {
-        // This returns a JSON or XML with the users
+    @PostMapping
+    @ResponseBody
+    public Customer create(@RequestBody final Customer customer) {
 
-        Customer customer = customerRepository.findById(Integer.parseInt(id)).orElse(null);
-
-        return customer;
-    }
-
-    @PostMapping("/customers")
-    public Customer create(@RequestParam String name, @RequestParam String inn, Map<String, Object> model) {
-
-        Customer customer = new Customer(name, inn);
         customerRepository.save(customer);
-
-        //model.put("customers", customers);
         return customer;
     }
 
-    @PutMapping("{id}")
-    public Customer update(@PathVariable(required = true) String id, Map<String, Object> model) {
+    @PutMapping
+    @ResponseBody
+    public Customer update(@RequestBody final Customer customer) {
 
-        Integer idInteger = Integer.parseInt(id);
-        Customer customer = null;
-        if(customerRepository.existsById(idInteger)) {
-
-            customer = customerRepository.findById(Integer.parseInt(id)).orElse(null);
-            customerRepository.save(customer);
-        }
-
-
+        customerRepository.save(customer);
         return customer;
     }
 
-    @DeleteMapping("{id}")
-    public void remove(@PathVariable(required = true) String id) {
-        if(!id.equals("")) {
+    @DeleteMapping
+    public void remove(@RequestBody(required = true) final Customer customer) {
 
-            Integer idInteger = Integer.parseInt(id);
-            if(customerRepository.existsById(idInteger)) {
-                customerRepository.deleteById(idInteger);
-            }
-        }
+        customerRepository.delete(customer);
+
     }
 }
